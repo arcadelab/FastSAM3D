72c135f19181cd866cdec02f8879e9b9bf8ee5c3
Jingxing Li <152577071+skill-diver@users.noreply.github.com>
2024-03-02 18:56:37 -0500

    delete redundent

--- segment_anything/modeling/Swin3D/sparse_dl/knn/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-"""
-from .knn import KNN 
-
-__all__ = [
-    'KNN',
-]
--- segment_anything/modeling/Swin3D/src/knn/knn_api.cpp
+++
@@ -1,6 +0,0 @@
-#include <torch/extension.h>
-#include "knn_cuda_kernel.h"
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("knn_cuda", &knn_cuda, "knn_cuda");
-}
