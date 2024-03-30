62b71b3a52c554ee0e5c6cb46837ed43fd60d566
Li <152577071+skill-diver@users.noreply.github.com>
2024-03-29 21:35:37 -0400

    Update preparelabel.sh

--- preparelabel.sh
+++ preparelabel.sh
@@ -1,5 +1,5 @@
 python ./preparelabel.py \
-  --data_validation_path "./data/train" \  # Used for teacher model's encoder to generate logits, 
+  --data_train_path "./data/train" \  # Used for teacher model's encoder to generate logits, 
   --label_path_base "./data/augmentation/label" \
   --train_path_base "./data/augmentation/images" \
   --checkpoint_path "./ckpt/sam_med3d_turbo.pth" \
